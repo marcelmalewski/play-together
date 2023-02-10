49425e04f2cf6629f10c742fbd98a34ca84a56aa
marcel <mmalewski9@gmail.com>
2023-02-10 18:09:53 +0100

    Added api

---
+++ api/src/main/java/com/marcelmalewski/playtogetherapi/PlayTogetherApiApplication.java
@@ -0,0 +1,13 @@
+package com.marcelmalewski.playtogetherapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayTogetherApiApplication {
+
+   public static void main(String[] args) {
+      SpringApplication.run(PlayTogetherApiApplication.class, args);
+   }
+
+}
