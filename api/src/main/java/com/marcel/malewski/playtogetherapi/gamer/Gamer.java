44436dd97af1a0e6b0991120ee779d44397fd08a
marcel <mmalewski9@gmail.com>
2023-02-12 14:45:47 +0100

    Added gamer classes

---
+++ api/src/main/java/com/marcel/malewski/playtogetherapi/gamer/Gamer.java
@@ -0,0 +1,12 @@
+package com.marcel.malewski.playtogetherapi.gamer;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+public class Gamer {
+   private Long id;
+   private String firstName;
+   private String lastName;
+}
