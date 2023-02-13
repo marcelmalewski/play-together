def1d1655784d0085ec3f11e383d55a1644bf084
marcel <mmalewski9@gmail.com>
2023-02-13 22:15:26 +0100

    Updated gamer class

--- api/src/main/java/com/marcel/malewski/playtogetherapi/gamer/Gamer.java
+++ api/src/main/java/com/marcel/malewski/playtogetherapi/gamer/Gamer.java
@@ -9,4 +9,8 @@ public class Gamer {
    private Long id;
    private String firstName;
    private String lastName;
+   private String email;
+   private String login;
+   private String nickname;
+   private String password;
 }
