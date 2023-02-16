e32a2491bc7406dc6622cb76267fe0d7730e52fc
marcel <mmalewski9@gmail.com>
2023-02-16 20:55:55 +0100

    Added gamer entity

--- api/src/main/java/com/marcel/malewski/playtogetherapi/gamer/Gamer.java
+++ api/src/main/java/com/marcel/malewski/playtogetherapi/gamer/Gamer.java
@@ -1,11 +1,19 @@
 package com.marcel.malewski.playtogetherapi.gamer;
 
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
+import lombok.*;
 
 @AllArgsConstructor
 @NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "gamer")
 public class Gamer {
+   @Id
+   @SequenceGenerator(name = "gamer_sequence", sequenceName = "gamer_sequence", allocationSize = 1)
+   @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "gamer_sequence")
    private Long id;
    private String firstName;
    private String lastName;
