d0513b0b629eca4bc1e1807d7b667edcf2406667
marcel <mmalewski9@gmail.com>
2023-02-19 12:34:06 +0100

    updated GamerRepository

--- api/src/main/java/com/marcel/malewski/playtogetherapi/gamer/GamerRepository.java
+++ api/src/main/java/com/marcel/malewski/playtogetherapi/gamer/GamerRepository.java
@@ -1,4 +1,8 @@
 package com.marcel.malewski.playtogetherapi.gamer;
 
-public class GamerRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GamerRepository extends JpaRepository<Gamer, Long> {
 }
