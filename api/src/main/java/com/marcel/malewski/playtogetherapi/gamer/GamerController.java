cd8843e4a199dc6066481b6a488b81ad282872ff
marcel <mmalewski9@gmail.com>
2023-02-19 12:33:51 +0100

    updated GamerController

--- api/src/main/java/com/marcel/malewski/playtogetherapi/gamer/GamerController.java
+++ api/src/main/java/com/marcel/malewski/playtogetherapi/gamer/GamerController.java
@@ -1,47 +1,63 @@
 package com.marcel.malewski.playtogetherapi.gamer;
 
+import com.marcel.malewski.playtogetherapi.gamer.dto.GamerResponseDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping(path = "/v1/gamers")
-@Tag(name = "Meetings", description = "Meetings API")
+@Tag(name = "Gamers", description = "Gamers API")
 public class GamerController {
+   private final GamerService gamerService;
+
+   public GamerController(GamerService gamerService) {
+      this.gamerService = gamerService;
+   }
+
    @GetMapping
    @Operation(summary = "Get all gamers")
-   public ResponseEntity<String> getAllGamers() {
-      String result = "All gamers3333";
+   public ResponseEntity<List<GamerResponseDto>> getAllGamers() {
+      List<GamerResponseDto> result = gamerService.getAllGamers();
       return new ResponseEntity<>(result, HttpStatus.OK);
    }
 
    @GetMapping("/{id}")
    @Operation(summary = "Get gamer by id")
-   public ResponseEntity<String> getGamer(@PathVariable String id) {
-      String result = "gamer13213";
+   public ResponseEntity<GamerResponseDto> getGamer(@PathVariable Long id) {
+      GamerResponseDto result = gamerService.getGamer(id);
       return new ResponseEntity<>(result, HttpStatus.OK);
    }
 
    @PostMapping
    @Operation(summary = "Create new gamer")
-   public ResponseEntity<String> createGamer() {
-      String result = "gamer";
+   public ResponseEntity<GamerResponseDto> createGamer() {
+      GamerResponseDto result = gamerService.createGamer();
       return new ResponseEntity<>(result, HttpStatus.OK);
    }
 
    @PutMapping
-   @Operation(summary = "Update gamer")
-   public ResponseEntity<String> updateGamer() {
-      String result = "gamer1";
+   @Operation(summary = "Update gamer by id")
+   public ResponseEntity<GamerResponseDto> updateGamer() {
+      GamerResponseDto result = gamerService.updateGamer();
       return new ResponseEntity<>(result, HttpStatus.OK);
    }
 
-   @PatchMapping
-   @Operation(summary = "Update gamer")
-   public ResponseEntity<String> partiallyUpdateGamer() {
-      String result = "gamer2";
-      return new ResponseEntity<>(result, HttpStatus.OK);
+//   @PatchMapping
+//   @Operation(summary = "Partially update gamer")
+//   public ResponseEntity<String> partiallyUpdateGamer() {
+//      String result = "gamer2";
+//      return new ResponseEntity<>(result, HttpStatus.OK);
+//   }
+
+   @DeleteMapping("/{id}")
+   @Operation(summary = "Delete gamer by id")
+   public ResponseEntity<Void> deleteGamer(@PathVariable Long id) {
+      gamerService.deleteGamer(id);
+      return new ResponseEntity<>(HttpStatus.OK);
    }
 }
