4229b72b48b3f782f9bf7284a6c0384c02b8c2c5
Marcel Malewski <mmalewski9@gmail.com>
2023-05-15 19:28:39 +0200

    updated security

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -20,17 +20,19 @@ repositories {
 }
 
 //TODO co oznaczają te imp compileOny annot itd. ?
+//TODO co robi spring devtools
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.mapstruct:mapstruct:1.5.3.Final")
-    implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.0.2")
     compileOnly("org.projectlombok:lombok")
     runtimeOnly("org.postgresql:postgresql")
     annotationProcessor("org.projectlombok:lombok")
     annotationProcessor("org.mapstruct:mapstruct-processor:1.5.3.Final")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
--- api/src/main/java/com/marcel/malewski/playtogetherapi/config/SecurityConfiguration.java
+++ api/src/main/java/com/marcel/malewski/playtogetherapi/config/SecurityConfiguration.java
@@ -3,11 +3,15 @@ package com.marcel.malewski.playtogetherapi.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
+//TODO dodać profile
+//TODO co to dokladnie stateless session
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
@@ -19,13 +23,18 @@ public class SecurityConfiguration {
 			.csrf()
 			.disable()
 			.authorizeHttpRequests()
-			.requestMatchers("")
+			.requestMatchers("/")
 			.permitAll()
 			.anyRequest()
 			.authenticated()
 			.and()
 			.sessionManagement()
 			.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+			.and()
+			.formLogin()
+			.and()
+			.exceptionHandling()
+			.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED));
 
 		return http.build();
 	}
