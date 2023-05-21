b684cefb2414f47dbf9c6b35b552e66396909d18
Marcel Malewski <mmalewski9@gmail.com>
2023-05-21 23:31:41 +0200

    added mapper

--- api/src/main/java/com/marcel/malewski/playtogetherapi/gamer/GamerMapper.java
+++ api/src/main/java/com/marcel/malewski/playtogetherapi/gamer/GamerMapper.java
@@ -4,13 +4,10 @@ import org.mapstruct.Mapper;
 
 import java.util.List;
 
-public class GamerMapper {
-}
-
-//@Mapper(componentModel = "spring")
-//public interface GamerMapper {
-//	GamerResponseDto mapToGamerResponseDto(Gamer gamer);
+@Mapper(componentModel = "spring")
+public interface GamerMapper {
+	GamerResponseDto mapToGamerResponseDto(Gamer gamer);
 //	List<GamerResponseDto> mapToGamerList(List<Gamer> gamers);
 //	Gamer mapToGamer(GamerCreateRequestDto gamerCreateRequestDto);
 //	Gamer mapToGamer(GamerUpdateRequestDto gamerUpdateRequestDto);
-//}
+}
