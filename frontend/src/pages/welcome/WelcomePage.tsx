1afac49b5599a2e4d25406b9726f4b02a163b4bf
Marcel Malewski <mmalewski9@gmail.com>
2023-06-06 22:03:36 +0200

    welcome fixed

--- frontend/src/pages/welcome/WelcomePage.tsx
+++ frontend/src/pages/welcome/WelcomePage.tsx
@@ -2,20 +2,22 @@ import { Link } from "react-router-dom";
 
 export function WelcomePage() {
   return (
-    <div className="flex h-screen w-screen flex-col items-center justify-center">
-      <p>welcome</p>
-      <Link
-        to="/login"
-        className="inline-block flex h-12 w-40 items-center justify-center rounded-md bg-base-button text-2xl duration-200 hover:bg-base-button-hov"
-      >
-        Login
-      </Link>
-      <Link
-        to="/register"
-        className="mt-10 inline-block flex h-12 w-40 items-center justify-center rounded-md bg-base-button text-2xl duration-200 hover:bg-base-button-hov"
-      >
-        Register
-      </Link>
+    <div className="flex h-screen w-screen flex-col items-center justify-center gap-40">
+      <p className="text-3xl">Welcome</p>
+      <div className="flex flex-col items-center justify-center gap-4">
+        <Link
+          to="/login"
+          className="inline-block flex h-12 w-40 items-center justify-center rounded-md bg-base-button text-2xl duration-200 hover:bg-base-button-hov"
+        >
+          Login
+        </Link>
+        <Link
+          to="/register"
+          className="inline-block flex h-12 w-40 items-center justify-center rounded-md bg-base-button text-2xl duration-200 hover:bg-base-button-hov"
+        >
+          Register
+        </Link>
+      </div>
     </div>
   );
 }
