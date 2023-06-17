6b7e3ca52d4e77f40336b27a89f8b472f9e595c8
Marcel Malewski <mmalewski9@gmail.com>
2023-06-17 11:17:27 +0200

    added repositories for platform and role

---
+++ api/src/main/java/com/marcel/malewski/playtogetherapi/gamerrole/GamerRoleRepository.java
@@ -0,0 +1,6 @@
+package com.marcel.malewski.playtogetherapi.gamerrole;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GamerRoleRepository extends JpaRepository<GamerRole, Long> {
+}
---
+++ api/src/main/java/com/marcel/malewski/playtogetherapi/platform/PlatformRepository.java
@@ -0,0 +1,6 @@
+package com.marcel.malewski.playtogetherapi.platform;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PlatformRepository extends JpaRepository<Platform, Long> {
+}
