90d6ee4c392a90555361445ee2258b8ad31165b2
Marcel Malewski <mmalewski9@gmail.com>
2023-07-11 23:08:25 +0200

    updated exceptions

--- api/src/main/java/com/marcel/malewski/playtogetherapi/exception/RestExceptionHandler.java
+++ api/src/main/java/com/marcel/malewski/playtogetherapi/exception/RestExceptionHandler.java
@@ -7,11 +7,13 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.List;
+
 //TODO dodać tutaj logowanie tych exceptionow jako warningi
 @RestControllerAdvice
 public class RestExceptionHandler {
 
-	//TODO dokonczyc, pytanie kiedy taki wyjatek jest rzucany
+	//TODO dlaczego trzeba to obsłużyć to constraintViolationException
 	@ResponseStatus(value = HttpStatus.UNPROCESSABLE_ENTITY)
 	@ExceptionHandler(ConstraintViolationException.class)
 	public ExceptionResponse handleConstraintViolation(ConstraintViolationException constraintViolationException) {
@@ -24,6 +26,9 @@ public class RestExceptionHandler {
 	public ExceptionResponse handleMethodArgumentNotValidException(MethodArgumentNotValidException methodArgumentNotValidException) {
 		System.out.println(methodArgumentNotValidException.getBindingResult().getGlobalErrors());
 		System.out.println(methodArgumentNotValidException.getBindingResult().getFieldErrors());
+		List<String> globalErrorsMessages = new
+		String exceptionMessage = "";
+
 		ExceptionResponse exceptionResponse = new ExceptionResponse("method validation");
 		return exceptionResponse;
 	}
