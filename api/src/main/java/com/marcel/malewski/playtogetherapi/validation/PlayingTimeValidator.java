cf811b564f6003e8047100655221a706ea24e585
Marcel Malewski <mmalewski9@gmail.com>
2023-07-14 23:37:12 +0200

    updated PlayingTimeValidator

--- api/src/main/java/com/marcel/malewski/playtogetherapi/validation/PlayingTimeValidator.java
+++ api/src/main/java/com/marcel/malewski/playtogetherapi/validation/PlayingTimeValidator.java
@@ -9,14 +9,13 @@ import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 import java.time.format.ResolverStyle;
 
+//TODO sprobować użyć bezpośrednio formatter.parse()
 public class PlayingTimeValidator implements ConstraintValidator<ValidatePlayingTime, GamerRegisterRequestDto> {
 	@Override
 	public boolean isValid(GamerRegisterRequestDto gamerRegisterRequestDto, ConstraintValidatorContext context) {
 		DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm").withResolverStyle(ResolverStyle.STRICT);
-		String playingTimeStart = gamerRegisterRequestDto.playingTimeStart();
-		String playingTimeEnd = gamerRegisterRequestDto.playingTimeEnd();
 
-		System.out.println("yes");
+		String playingTimeStart = gamerRegisterRequestDto.playingTimeStart();
 		LocalTime playingTimeStartAsDate;
 		try {
 			playingTimeStartAsDate = LocalTime.parse(playingTimeStart, timeFormatter);
@@ -24,14 +23,13 @@ public class PlayingTimeValidator implements ConstraintValidator<ValidatePlaying
 			return true;
 		}
 
+		String playingTimeEnd = gamerRegisterRequestDto.playingTimeEnd();
 		LocalTime playingTimeEndAsDate;
 		try {
 			playingTimeEndAsDate = LocalTime.parse(playingTimeEnd, timeFormatter);
 		} catch (DateTimeParseException exception) {
 			return true;
 		}
-
-		System.out.println("yes");
 
 		return playingTimeEndAsDate.isAfter(playingTimeStartAsDate);
 	}
