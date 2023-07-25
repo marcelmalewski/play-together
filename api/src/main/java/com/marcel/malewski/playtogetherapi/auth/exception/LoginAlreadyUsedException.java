de0ef517bffe7b4b8b7f71c86790d7b195258768
Marcel Malewski <mmalewski9@gmail.com>
2023-07-25 20:02:58 +0200

    added LoginAlreadyUsedException

--- api/src/main/java/com/marcel/malewski/playtogetherapi/auth/exception/LoginAlreadyUsedException.java
+++ api/src/main/java/com/marcel/malewski/playtogetherapi/auth/exception/LoginAlreadyUsedException.java
@@ -4,9 +4,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value = HttpStatus.BAD_REQUEST)
-public class AlreadyAuthenticatedException extends RuntimeException {
-	public AlreadyAuthenticatedException() {
-		super("User is already authenticated");
+public class LoginAlreadyUsedException extends RuntimeException {
+	public LoginAlreadyUsedException(String login) {
+		super("Login is already used, login: " + login);
 	}
 }
-
