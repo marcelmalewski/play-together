67426dd396cbf80ece876aafa523246fd88f70ae
Marcel Malewski <mmalewski9@gmail.com>
2023-07-25 22:57:27 +0200

    added GivenPlatformDoesNotExistException

--- api/src/main/java/com/marcel/malewski/playtogetherapi/auth/exception/GivenPlatformDoesNotExistException.java
+++ api/src/main/java/com/marcel/malewski/playtogetherapi/auth/exception/GivenPlatformDoesNotExistException.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value = HttpStatus.BAD_REQUEST)
 public class GivenPlatformDoesNotExistException extends RuntimeException {
-	public GivenPlatformDoesNotExistException(String platform) {
-		super("Given platform does not exist, platform: " + platform);
+	public GivenPlatformDoesNotExistException(Long platformId) {
+		super("Given platform does not exist, platformId: " + platformId);
 	}
 }
