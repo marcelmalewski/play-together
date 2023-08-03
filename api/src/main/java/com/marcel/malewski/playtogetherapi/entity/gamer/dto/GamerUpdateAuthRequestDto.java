5016e31280f2d7e829d22f2ff5f71baef3d055e8
Marcel Malewski <mmalewski9@gmail.com>
2023-08-03 22:55:51 +0200

    finished updateGamerAuth

--- api/src/main/java/com/marcel/malewski/playtogetherapi/entity/gamer/dto/GamerUpdateAuthRequestDto.java
+++ api/src/main/java/com/marcel/malewski/playtogetherapi/entity/gamer/dto/GamerUpdateAuthRequestDto.java
@@ -1,6 +1,6 @@
 package com.marcel.malewski.playtogetherapi.entity.gamer.dto;
 
-//TODO dodać walidacje
+//TODO dodać walidacje, obowiązkowe jest tylko currentPassword
 public record GamerUpdateAuthRequestDto(
 	String email,
 	String newPassword,
