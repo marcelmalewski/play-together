bffb51eeeba0ce53bdf6debb256f43b58eed162c
Marcel Malewski <mmalewski9@gmail.com>
2023-08-07 23:43:12 +0200

    refactored CustomAccessDeniedHandler

--- api/src/main/java/com/marcel/malewski/playtogetherapi/auth/CustomAccessDeniedHandler.java
+++ api/src/main/java/com/marcel/malewski/playtogetherapi/auth/CustomAccessDeniedHandler.java
@@ -6,27 +6,27 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.access.AccessDeniedHandler;
 
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.Collection;
+import java.util.stream.Collectors;
 
 public class CustomAccessDeniedHandler implements AccessDeniedHandler {
 
 	public static final Logger LOG = LoggerFactory.getLogger(CustomAccessDeniedHandler.class);
 
 	@Override
-	public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException exc) throws IOException {
+	public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException exc) {
 		Authentication auth = SecurityContextHolder.getContext()
 			.getAuthentication();
 		if (auth != null) {
 			LOG.warn("User: " + auth.getName() + " attempted to access the protected URL: " + request.getRequestURI());
-			LOG.warn("User roles: " + Arrays.toString(new Collection[]{auth.getAuthorities()}));
-		}
+			String userRolesAString = auth.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.joining(", "));
+			LOG.warn("User roles: " + userRolesAString);
 
-		//TODO dodać wypisywanie wymaganych ról
-		response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+			//TODO dodać wypisywanie wymaganych ról
+			response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+		}
 	}
 }
