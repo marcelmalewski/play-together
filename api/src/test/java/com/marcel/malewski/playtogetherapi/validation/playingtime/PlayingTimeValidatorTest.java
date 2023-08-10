5aba1677c7a1ab7001824c95fb1a55efffd1d326
Marcel Malewski <mmalewski9@gmail.com>
2023-08-10 19:58:45 +0200

    updated PlayingTimeValidatorTest with constants

--- api/src/test/java/com/marcel/malewski/playtogetherapi/validation/playingtime/PlayingTimeValidatorTest.java
+++ api/src/test/java/com/marcel/malewski/playtogetherapi/validation/playingtime/PlayingTimeValidatorTest.java
@@ -1,6 +1,7 @@
 package com.marcel.malewski.playtogetherapi.validation.playingtime;
 
 import com.marcel.malewski.playtogetherapi.security.register.GamerRegisterRequestDto;
+import com.marcel.malewski.playtogetherapi.validation.ValidGamerRegisterRequestDto;
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.Validation;
 import jakarta.validation.Validator;
@@ -8,9 +9,9 @@ import jakarta.validation.ValidatorFactory;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
 import java.util.Set;
 
+import static com.marcel.malewski.playtogetherapi.validation.ValidationConstants.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 //TODO poprawic na test tylko specyficznego validatora i wtedy dodac test z nullami
@@ -28,15 +29,7 @@ class PlayingTimeValidatorTest {
   //TODO w paru testach validatorow jest jeden bardzo podobny test zweryfikowac czy to okej
   @Test
   void shouldFindNoViolationsWhenPlayingTimesAreValid() {
-    registerRequestDto = new GamerRegisterRequestDto(
-      "username",
-      "test1234534563456",
-      "yes@yes.com",
-      "2000-01-01",
-      "15:00",
-      "18:00",
-      List.of(1L)
-    );
+    registerRequestDto = ValidGamerRegisterRequestDto.getValidGamerRegisterRequestDto();
 
     Set<ConstraintViolation<GamerRegisterRequestDto>> violations = validator.validate(registerRequestDto);
     assertEquals(0, violations.size());
@@ -45,13 +38,13 @@ class PlayingTimeValidatorTest {
   @Test
   void shouldFindViolationWhenEndTimeIsBeforeStartTime() {
     registerRequestDto = new GamerRegisterRequestDto(
-      "username",
-      "test1234534563456",
-      "yes@yes.com",
-      "2000-01-01",
+      LOGIN,
+      PASSWORD,
+      EMAIL,
+      BIRTH_DATE,
       "15:00",
       "14:00",
-      List.of(1L)
+      PLATFORMS_IDS
     );
 
     Set<ConstraintViolation<GamerRegisterRequestDto>> violations = validator.validate(registerRequestDto);
