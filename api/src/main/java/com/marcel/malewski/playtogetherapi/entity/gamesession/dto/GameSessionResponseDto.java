3932f83b8a91e471e9093d39285f4ee5f6e50dfd
Marcel Malewski <mmalewski9@gmail.com>
2023-08-13 11:57:05 +0200

    added GameSessionResponseDto

--- api/src/main/java/com/marcel/malewski/playtogetherapi/entity/gamesession/dto/GameSessionResponseDto.java
+++ api/src/main/java/com/marcel/malewski/playtogetherapi/entity/gamesession/dto/GameSessionResponseDto.java
@@ -1,4 +1,25 @@
 package com.marcel.malewski.playtogetherapi.entity.gamesession.dto;
 
-public class GameSessionResponseDto {
+import com.marcel.malewski.playtogetherapi.enums.PrivacyLevelEnum;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public record GameSessionResponseDto(
+	Long id,
+	String name,
+	boolean isCompetitive,
+	PrivacyLevelEnum accessType,
+	LocalDate date,
+	int numberOfMembers,
+	int maxMembers,
+	int minAge,
+	boolean isCurrentGamerMember,
+	String description,
+	String pendingMembers,
+	String availabilityTimes,
+	String creatorName,
+	String gameName,
+	List<String> platforms
+) {
 }
