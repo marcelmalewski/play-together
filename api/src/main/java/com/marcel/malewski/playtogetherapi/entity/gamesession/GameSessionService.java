69fbaeabab92cd7d21d20ceca3b90d7d3a7100df
Marcel Malewski <mmalewski9@gmail.com>
2023-08-13 12:44:12 +0200

    finished getGameSession

--- api/src/main/java/com/marcel/malewski/playtogetherapi/entity/gamesession/GameSessionService.java
+++ api/src/main/java/com/marcel/malewski/playtogetherapi/entity/gamesession/GameSessionService.java
@@ -1,6 +1,7 @@
 package com.marcel.malewski.playtogetherapi.entity.gamesession;
 
 import com.marcel.malewski.playtogetherapi.entity.gamesession.dto.GameSessionResponseDto;
+import com.marcel.malewski.playtogetherapi.entity.gamesession.exception.GameSessionNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -17,5 +18,10 @@ public class GameSessionService {
 
 	public List<GameSessionResponseDto> findAllGameSessions() {
 		return gameSessionRepository.findAll().stream().map(gameSessionMapper::toGameSessionResponseDto).toList();
+	}
+
+	public GameSessionResponseDto getGameSession(long id) {
+		GameSession gameSession = gameSessionRepository.findById(id).orElseThrow(() -> new GameSessionNotFoundException(id));
+		return gameSessionMapper.toGameSessionResponseDto(gameSession);
 	}
 }
