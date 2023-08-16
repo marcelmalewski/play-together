941471608f665c3bccadb46b4775998e6b9b8406
Marcel Malewski <mmalewski9@gmail.com>
2023-08-16 22:43:48 +0200

    refactored PlatformService

--- api/src/main/java/com/marcel/malewski/playtogetherapi/entity/platform/PlatformService.java
+++ api/src/main/java/com/marcel/malewski/playtogetherapi/entity/platform/PlatformService.java
@@ -1,8 +1,6 @@
 package com.marcel.malewski.playtogetherapi.entity.platform;
 
 import com.marcel.malewski.playtogetherapi.entity.platform.exception.GivenPlatformDoesNotExistException;
-import com.marcel.malewski.playtogetherapi.entity.platform.exception.PlatformNotFoundException;
-import jakarta.validation.constraints.NotNull;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -19,28 +17,11 @@ public class PlatformService {
 		return platformRepository.findAll();
 	}
 
-	public Platform getPlatformReference(long id) {
-		if (!platformRepository.existsById(id)) {
-			throw new PlatformNotFoundException(id);
-		}
-
-		return platformRepository.getReferenceById(id);
-	}
-
 	public Platform getReferenceOfGivenPlatform(long id) {
 		if (!platformRepository.existsById(id)) {
 			throw new GivenPlatformDoesNotExistException(id);
 		}
 
 		return platformRepository.getReferenceById(id);
-	}
-
-	//TODO może lepsza nazwa
-	public void throwExceptionIfGivenPlatformDoesNotExist(@NotNull List<Long> platformsIds) {
-		platformsIds.forEach(platformId -> {
-			if (!platformRepository.existsById(platformId)) {
-				throw new GivenPlatformDoesNotExistException(platformId);
-			}
-		});
 	}
 }
