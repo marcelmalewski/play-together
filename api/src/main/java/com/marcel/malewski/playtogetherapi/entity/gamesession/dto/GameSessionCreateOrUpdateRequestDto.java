11e642974d805c9e50dd6652a1ec87bb4a8663b5
Marcel Malewski <mmalewski9@gmail.com>
2023-09-13 21:33:04 +0200

    updated dto

--- api/src/main/java/com/marcel/malewski/playtogetherapi/entity/gamesession/dto/GameSessionCreateOrUpdateRequestDto.java
+++ api/src/main/java/com/marcel/malewski/playtogetherapi/entity/gamesession/dto/GameSessionCreateOrUpdateRequestDto.java
@@ -1,11 +1,15 @@
 package com.marcel.malewski.playtogetherapi.entity.gamesession.dto;
 
 import com.marcel.malewski.playtogetherapi.enums.PrivacyLevel;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
 import java.util.List;
 
 public record GameSessionCreateOrUpdateRequestDto(
+	@Size(min = 3, max = 20)
+	@NotBlank
 	String name,
 	PrivacyLevel visibilityType,
 	boolean isCompetitive,
