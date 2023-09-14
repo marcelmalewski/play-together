6f1d8f7e8a5a457bdcfc1d8bc4048b5bcb9327fd
Marcel Malewski <mmalewski9@gmail.com>
2023-09-14 22:00:46 +0200

    handled invalid sort RequestParam

--- api/src/main/java/com/marcel/malewski/playtogetherapi/exception/RestExceptionHandler.java
+++ api/src/main/java/com/marcel/malewski/playtogetherapi/exception/RestExceptionHandler.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import java.util.List;
 
@@ -37,5 +38,11 @@ public class RestExceptionHandler {
 		String separator = globalErrorMessages.isEmpty() ? "" : ";";
 		String allErrorMessages = String.join("; ", globalErrorMessages) + separator + String.join("; ", fieldErrorMessages);
 		return new ExceptionResponse(allErrorMessages);
+	}
+
+	@ResponseStatus(value = HttpStatus.BAD_REQUEST)
+	@ExceptionHandler(MethodArgumentTypeMismatchException.class)
+	public ExceptionResponse handleMethodArgumentNotValidException(MethodArgumentTypeMismatchException exception) {
+		return new ExceptionResponse(exception.getCause().getCause().getMessage());
 	}
 }
