84fe7325abbf17eb9a15723f99798b6f15493794
Marcel Malewski <mmalewski9@gmail.com>
2023-09-15 21:14:02 +0200

    added Enum validator

---
+++ api/src/main/java/com/marcel/malewski/playtogetherapi/validation/enums/ValidEnum.java
@@ -0,0 +1,20 @@
+package com.marcel.malewski.playtogetherapi.validation.enums;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.*;
+
+@Documented
+@Constraint(validatedBy = EnumValidator.class)
+@Target({ElementType.METHOD, ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ValidEnum {
+	Class<? extends Enum<?>> enumClass();
+
+	String message() default "must be any of enum {enumClass}";
+
+	Class<?>[] groups() default {};
+
+	Class<? extends Payload>[] payload() default {};
+}
