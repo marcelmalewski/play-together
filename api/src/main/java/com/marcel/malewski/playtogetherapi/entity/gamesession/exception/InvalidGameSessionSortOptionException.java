8d339d62aeb5af497f35e9ca267fbf47f72e3a08
Marcel Malewski <mmalewski9@gmail.com>
2023-09-17 11:57:55 +0200

    updated InvalidGameSessionSortOptionException

--- api/src/main/java/com/marcel/malewski/playtogetherapi/entity/gamesession/exception/InvalidGameSessionSortOptionException.java
+++ api/src/main/java/com/marcel/malewski/playtogetherapi/entity/gamesession/exception/InvalidGameSessionSortOptionException.java
@@ -1,11 +1,17 @@
 package com.marcel.malewski.playtogetherapi.entity.gamesession.exception;
 
+import com.marcel.malewski.playtogetherapi.entity.gamesession.enums.GameSessionSortOption;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.Arrays;
 
 @ResponseStatus(value = HttpStatus.BAD_REQUEST)
 public class InvalidGameSessionSortOptionException extends RuntimeException {
 	public InvalidGameSessionSortOptionException(String gameSessionSortOption) {
-		super("Invalid game session sort option, sort: " + gameSessionSortOption);
+		super(
+			"Invalid game session sort option, sort: " + gameSessionSortOption + ". " +
+			"Valid values: "	+ Arrays.toString(GameSessionSortOption.values())
+		);
 	}
 }
