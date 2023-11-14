792d4899c2ef8a04b37e87f33b57352732d1732b
Marcel Malewski <mmalewski9@gmail.com>
2023-11-14 19:56:32 +0100

    added TODO

--- api/src/main/java/com/marcel/malewski/playtogetherapi/entity/gamerrole/GamerRoleName.java
+++ api/src/main/java/com/marcel/malewski/playtogetherapi/entity/gamerrole/GamerRoleName.java
@@ -2,6 +2,6 @@ package com.marcel.malewski.playtogetherapi.entity.gamerrole;
 
 public enum GamerRoleName {
 	ROLE_USER,
-	ROLE_MODERATOR,
-	ROLE_ADMIN
+	ROLE_MODERATOR,//TODO zarządza userami
+	ROLE_ADMIN //TODO przydziela role, zarządza adminami, jest może tylko jeden albo cos
 }
