6d9e4e2fa4f0bb7171f646ac90d1efe9e7a008c6
Marcel Malewski <mmalewski9@gmail.com>
2023-11-15 21:09:38 +0100

    Changed platform name enum

--- api/src/test/java/com/marcel/malewski/playtogetherapi/util/TestPlatformCreator.java
+++ api/src/test/java/com/marcel/malewski/playtogetherapi/util/TestPlatformCreator.java
@@ -1,12 +1,12 @@
 package com.marcel.malewski.playtogetherapi.util;
 
 import com.marcel.malewski.playtogetherapi.entity.platform.Platform;
-import com.marcel.malewski.playtogetherapi.entity.platform.PlatformName;
+import com.marcel.malewski.playtogetherapi.entity.platform.BasicPlatformName;
 
 import java.util.List;
 
 public final class TestPlatformCreator {
-	private static final String PLATFORM_NAME = PlatformName.PC.name();
+	private static final String PLATFORM_NAME = BasicPlatformName.PC.name();
 	private static final long PLATFORM_ID = 1L;
 
 	private TestPlatformCreator() {
