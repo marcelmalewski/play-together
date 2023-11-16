73f1aced88c63cb44283f772a9097393e987a27e
Marcel Malewski <mmalewski9@gmail.com>
2023-11-16 20:32:36 +0100

    TODO done

--- api/src/main/java/com/marcel/malewski/playtogetherapi/entity/gamer/dto/GamerUpdateAuthenticationDataRequestDto.java
+++ api/src/main/java/com/marcel/malewski/playtogetherapi/entity/gamer/dto/GamerUpdateAuthenticationDataRequestDto.java
@@ -3,8 +3,6 @@ package com.marcel.malewski.playtogetherapi.entity.gamer.dto;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
-//TODO mandatory is only currentPassword
-//TODO newPassword and email can be null
 public record GamerUpdateAuthenticationDataRequestDto(
 	@Email
 	String email,
