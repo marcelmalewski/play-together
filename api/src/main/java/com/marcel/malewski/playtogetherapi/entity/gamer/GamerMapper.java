f1228af7f65c5b302c9e1152d3d569ff186f6a4c
Marcel Malewski <mmalewski9@gmail.com>
2023-11-16 20:48:34 +0100

    refactored duplicates

--- api/src/main/java/com/marcel/malewski/playtogetherapi/entity/gamer/GamerMapper.java
+++ api/src/main/java/com/marcel/malewski/playtogetherapi/entity/gamer/GamerMapper.java
@@ -1,69 +1,82 @@
 package com.marcel.malewski.playtogetherapi.entity.gamer;
 
-import com.marcel.malewski.playtogetherapi.entity.game.Game;
 import com.marcel.malewski.playtogetherapi.entity.gamer.dto.GamerPrivateResponseDto;
 import com.marcel.malewski.playtogetherapi.entity.gamer.dto.GamerPublicResponseDto;
-import com.marcel.malewski.playtogetherapi.entity.genre.Genre;
-import com.marcel.malewski.playtogetherapi.entity.platform.Platform;
 import org.mapstruct.Mapper;
 
 import java.time.LocalDate;
 import java.time.Period;
 import java.util.List;
 
-//TODO is this duplicate a problem?
 @Mapper(componentModel = "spring")
 public abstract class GamerMapper {
-	public GamerPrivateResponseDto toGamerPrivateResponseDto(Gamer gamer) {
-		if(gamer == null) {
-			return null;
-		}
 
-		List<String> platformsNames = gamer.getPlatforms().stream().map(Platform::getName).toList();
-		List<String> gamesNames = gamer.getFavouriteGames().stream().map(Game::getName).toList();
-		List<String> genresNames = gamer.getFavouriteGenres().stream().map(Genre::getName).toList();
+    public GamerPrivateResponseDto toGamerPrivateResponseDto(Gamer gamer) {
+        if (gamer == null) {
+            return null;
+        }
 
-		return new GamerPrivateResponseDto(
-			gamer.getId(),
-			gamer.getLogin(),
-			gamer.getEmail(),
-			gamer.getBirthdate(),
-			gamer.getBio(),
-			gamer.getAvatarUrl(),
-			gamer.getPlayingTimeStart(),
-			gamer.getPlayingTimeEnd(),
-			platformsNames,
-			gamesNames,
-			genresNames
-		);
-	}
+        List<String> platformsNames = getPlatformNames(gamer);
+        List<String> gamesNames = getGamesNames(gamer);
+        List<String> genresNames = getGenresNames(gamer);
 
-	public GamerPublicResponseDto toGamerPublicResponseDto(Gamer gamer) {
-		if(gamer == null) {
-			return null;
-		}
+        return new GamerPrivateResponseDto(
+                gamer.getId(),
+                gamer.getLogin(),
+                gamer.getEmail(),
+                gamer.getBirthdate(),
+                gamer.getBio(),
+                gamer.getAvatarUrl(),
+                gamer.getPlayingTimeStart(),
+                gamer.getPlayingTimeEnd(),
+                platformsNames,
+                gamesNames,
+                genresNames
+        );
+    }
 
-		List<String> platformsNames = gamer.getPlatforms().stream().map(Platform::getName).toList();
-		List<String> gamesNames = gamer.getFavouriteGames().stream().map(Game::getName).toList();
-		List<String> genresNames = gamer.getFavouriteGenres().stream().map(Genre::getName).toList();
-		int age = getAge(gamer);
+    public GamerPublicResponseDto toGamerPublicResponseDto(Gamer gamer) {
+        if (gamer == null) {
+            return null;
+        }
 
-		return new GamerPublicResponseDto(
-			gamer.getId(),
-			gamer.getLogin(),
-			age,
-			gamer.getBio(),
-			gamer.getAvatarUrl(),
-			gamer.getPlayingTimeStart(),
-			gamer.getPlayingTimeEnd(),
-			platformsNames,
-			gamesNames,
-			genresNames
-		);
-	}
+        List<String> platformsNames = getPlatformNames(gamer);
+        List<String> gamesNames = getGamesNames(gamer);
+        List<String> genresNames = getGenresNames(gamer);
+        int age = getAge(gamer);
 
-	private int getAge(Gamer gamer) {
-		LocalDate currentDay = LocalDate.now();
-		return Period.between(gamer.getBirthdate(), currentDay).getYears();
-	}
+        return new GamerPublicResponseDto(
+                gamer.getId(),
+                gamer.getLogin(),
+                age,
+                gamer.getBio(),
+                gamer.getAvatarUrl(),
+                gamer.getPlayingTimeStart(),
+                gamer.getPlayingTimeEnd(),
+                platformsNames,
+                gamesNames,
+                genresNames
+        );
+    }
+
+    private List<String> getNames(List<?> items) {
+        return items.stream().map(Object::toString).toList();
+    }
+
+    private List<String> getPlatformNames(Gamer gamer) {
+        return getNames(gamer.getPlatforms());
+    }
+
+    private List<String> getGamesNames(Gamer gamer) {
+        return getNames(gamer.getFavouriteGames());
+    }
+
+    private List<String> getGenresNames(Gamer gamer) {
+        return getNames(gamer.getFavouriteGenres());
+    }
+
+    private int getAge(Gamer gamer) {
+        LocalDate currentDay = LocalDate.now();
+        return Period.between(gamer.getBirthdate(), currentDay).getYears();
+    }
 }
