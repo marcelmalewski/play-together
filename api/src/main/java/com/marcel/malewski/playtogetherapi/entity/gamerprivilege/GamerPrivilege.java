087f774ff092e66f043f6ad07c113a036b253164
Marcel Malewski <mmalewski9@gmail.com>
2023-11-21 21:50:20 +0100

    Updated GamerPrivilege

--- api/src/main/java/com/marcel/malewski/playtogetherapi/entity/gamerprivilege/GamerPrivilege.java
+++ api/src/main/java/com/marcel/malewski/playtogetherapi/entity/gamerprivilege/GamerPrivilege.java
@@ -1,4 +1,13 @@
 package com.marcel.malewski.playtogetherapi.entity.gamerprivilege;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import org.springframework.data.annotation.Id;
+
 public class GamerPrivilege {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
 }
