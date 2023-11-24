98e5053a6f68df3eaa97430a15ef45ec419d7152
Marcel Malewski <mmalewski9@gmail.com>
2023-11-24 22:41:58 +0100

    Updated GamerPrivilege

---
+++ api/src/main/java/com/marcel/malewski/playtogetherapi/entity/gamerprivilege/GamerPrivilegeName.java
@@ -0,0 +1,8 @@
+package com.marcel.malewski.playtogetherapi.entity.gamerprivilege;
+
+//View, Search, Edit, Analyse, Manage
+public enum GamerPrivilegeName {
+    GAMER_EDIT,
+    GAMER_PUBLIC_VIEW,
+    GAMER_PRIVATE_VIEW
+}
