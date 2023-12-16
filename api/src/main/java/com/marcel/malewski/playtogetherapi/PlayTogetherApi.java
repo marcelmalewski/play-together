07107c7a627a4f9815d429ea1a193c850526ddd2
Marcel Malewski <mmalewski9@gmail.com>
2023-12-16 10:15:29 +0100

    test

--- api/src/main/java/com/marcel/malewski/playtogetherapi/PlayTogetherApi.java
+++ api/src/main/java/com/marcel/malewski/playtogetherapi/PlayTogetherApi.java
@@ -3,6 +3,7 @@ package com.marcel.malewski.playtogetherapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//test
 @SpringBootApplication
 public class PlayTogetherApi {
    public static void main(String[] args) {
