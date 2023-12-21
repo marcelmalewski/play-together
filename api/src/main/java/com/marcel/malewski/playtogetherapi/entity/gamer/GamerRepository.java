1821efc99ba7c27ee1220493a95ee5c1a70ee382
Marcel Malewski <mmalewski9@gmail.com>
2023-12-21 10:02:59 +0100

    Added findAllBuLoginIsLikeIgnoreCase

--- api/src/main/java/com/marcel/malewski/playtogetherapi/entity/gamer/GamerRepository.java
+++ api/src/main/java/com/marcel/malewski/playtogetherapi/entity/gamer/GamerRepository.java
@@ -3,11 +3,14 @@ package com.marcel.malewski.playtogetherapi.entity.gamer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface GamerRepository extends JpaRepository<Gamer, Long> {
+	List<Gamer> findAllByLoginIsLikeIgnoreCase(String gamerLogin);
+	Optional<Gamer> findByLoginOrEmail(String login, String email);
 	boolean existsByLogin(String login);
 	boolean existsByEmail(String email);
-	Optional<Gamer> findByLoginOrEmail(String login, String email);
 }
+
