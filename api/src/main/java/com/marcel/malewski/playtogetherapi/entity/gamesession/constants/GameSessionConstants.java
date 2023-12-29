9b1ae709c1c542492cfca6f9a44a67de4b585704
Marcel Malewski <mmalewski9@gmail.com>
2023-12-29 22:46:35 +0100

    Updated constants

--- api/src/main/java/com/marcel/malewski/playtogetherapi/entity/gamesession/constants/GameSessionConstants.java
+++ api/src/main/java/com/marcel/malewski/playtogetherapi/entity/gamesession/constants/GameSessionConstants.java
@@ -3,10 +3,11 @@ package com.marcel.malewski.playtogetherapi.entity.gamesession.constants;
 public final class GameSessionConstants {
 	public static final String DATE_FIELD = "date";
 	public static final String CREATED_AT_FIELD = "createdAt";
-	public static final int DEFAULT_PAGEABLE_PAGE = 0;
-	public static final String DEFAULT_PAGEABLE_PAGE_AS_STRING = "0";
-	public static final int DEFAULT_PAGEABLE_SIZE = 10;
-	public static final String DEFAULT_PAGEABLE_SIZE_AS_STRING = "10";
+	public static final int MIN_PAGE_NUMBER = 0;
+	public static final String MIN_PAGE_NUMBER_AS_STRING= "0";
+	public static final int MIN_PAGE_SIZE = 1;
+	public static final int MAX_PAGE_SIZE = 100;
+	public static final String DEFAULT_PAGE_SIZE_AS_STRING = "10";
 
 	private GameSessionConstants() {
 	}
