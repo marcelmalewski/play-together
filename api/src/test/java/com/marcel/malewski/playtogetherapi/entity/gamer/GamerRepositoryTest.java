9e4abbcf87f4db79507465133eb336beab38b4ea
Marcel Malewski <mmalewski9@gmail.com>
2023-12-30 22:06:09 +0100

    Fixed tests

--- api/src/test/java/com/marcel/malewski/playtogetherapi/entity/gamer/GamerRepositoryTest.java
+++ api/src/test/java/com/marcel/malewski/playtogetherapi/entity/gamer/GamerRepositoryTest.java
@@ -20,6 +20,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
@@ -104,9 +107,10 @@ public class GamerRepositoryTest {
 
 	@Test
 	void byLoginIsLikeIgnoreCaseShouldWork() {
-		List<Gamer> gamersList = gamerRepository.findAllByLoginIsLikeIgnoreCase("%Mro%");
+		Pageable pageable = PageRequest.of(0, 10);
+		Page<Gamer> gamerPage = gamerRepository.findAllByLoginIsLikeIgnoreCase("%Mro%", pageable);
 
-		assertThat(gamersList.size()).isEqualTo(2);
+		assertThat(gamerPage.getContent().size()).isEqualTo(2);
 	}
 
 	@Test
