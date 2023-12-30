d89e8e5907436591631167b6d0b888145ed9d42b
Marcel Malewski <mmalewski9@gmail.com>
2023-12-30 12:32:54 +0100

    MOved constants

---
+++ api/src/main/java/com/marcel/malewski/playtogetherapi/constant/PageableConstants.java
@@ -0,0 +1,16 @@
+package com.marcel.malewski.playtogetherapi.constant;
+
+public final class PageableConstants {
+	public static final int DEFAULT_PAGE_NUMBER = 0;
+	public static final String DEFAULT_PAGE_NUMBER_AS_STRING = "0";
+	public static final int MIN_PAGE_NUMBER = 0;
+
+	public static final int DEFAULT_PAGE_SIZE = 10;
+	public static final String DEFAULT_PAGE_SIZE_AS_STRING = "10";
+	public static final int MIN_PAGE_SIZE = 1;
+	public static final int MAX_PAGE_SIZE = 100;
+
+	private PageableConstants() {
+
+	}
+}
--- api/src/main/java/com/marcel/malewski/playtogetherapi/entity/gamesession/constants/GameSessionConstants.java
+++ api/src/main/java/com/marcel/malewski/playtogetherapi/entity/gamesession/constants/GameSessionConstants.java
@@ -3,11 +3,6 @@ package com.marcel.malewski.playtogetherapi.entity.gamesession.constants;
 public final class GameSessionConstants {
 	public static final String DATE_FIELD = "date";
 	public static final String CREATED_AT_FIELD = "createdAt";
-	public static final int MIN_PAGE_NUMBER = 0;
-	public static final String MIN_PAGE_NUMBER_AS_STRING= "0";
-	public static final int MIN_PAGE_SIZE = 1;
-	public static final int MAX_PAGE_SIZE = 100;
-	public static final String DEFAULT_PAGE_SIZE_AS_STRING = "10";
 
 	private GameSessionConstants() {
 	}
