06a5345259014e506cc143e4f6e2722a11be8d05
Marcel Malewski <mmalewski9@gmail.com>
2024-01-06 14:23:40 +0100

    Added GamerConstants

---
+++ api/src/main/java/com/marcel/malewski/playtogetherapi/entity/gamer/GamerConstants.java
@@ -0,0 +1,8 @@
+package com.marcel.malewski.playtogetherapi.entity.gamer;
+
+public final class GamerConstants {
+	public static final String LOGIN_FIELD = "login";
+
+	private GamerConstants() {
+	}
+}
