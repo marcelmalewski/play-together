4993879d857d2ae5c62e2e46b521ce69a5ee6c5a
Marcel Malewski <mmalewski9@gmail.com>
2024-01-06 20:39:19 +0100

    Added sorting in gamer service

--- api/src/main/java/com/marcel/malewski/playtogetherapi/entity/gamer/GamerService.java
+++ api/src/main/java/com/marcel/malewski/playtogetherapi/entity/gamer/GamerService.java
@@ -10,6 +10,7 @@ import com.marcel.malewski.playtogetherapi.entity.gamer.exception.EmailAlreadyUs
 import com.marcel.malewski.playtogetherapi.entity.gamer.exception.GamerNotFoundException;
 import com.marcel.malewski.playtogetherapi.entity.gamer.exception.LoginAlreadyUsedException;
 import com.marcel.malewski.playtogetherapi.entity.gamer.exception.NewPasswordSameAsPrevious;
+import com.marcel.malewski.playtogetherapi.entity.gamesession.enums.GameSessionSortOption;
 import com.marcel.malewski.playtogetherapi.entity.genre.Genre;
 import com.marcel.malewski.playtogetherapi.entity.genre.GenreService;
 import com.marcel.malewski.playtogetherapi.entity.platform.Platform;
@@ -27,6 +28,7 @@ import jakarta.validation.constraints.NotNull;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
@@ -54,6 +56,7 @@ public class GamerService {
 	private final SecurityHelper securityHelper;
 	private final PrincipalExtractor principalExtractor;
 
+	public static final GamerSortOption DEFAULT_GAMER_SORT_OPTION = GamerSortOption.LOGIN_ASC;
 
 	public GamerService(GamerRepository gamerRepository, PlatformService platformService, GenreService genreService, GamerMapper gamerMapper, GameService gameService, BCryptPasswordEncoder passwordEncoder, SecurityHelper securityHelper, PrincipalExtractor principalExtractor) {
 		this.gamerRepository = gamerRepository;
@@ -66,12 +69,15 @@ public class GamerService {
 		this.principalExtractor = principalExtractor;
 	}
 
-	public Page<GamerPublicResponseDto> findAllGamersPublicInfo(@Min(MIN_PAGE_NUMBER) Integer pageNumber, @Min(MIN_PAGE_SIZE) @Max(MAX_PAGE_SIZE) Integer pageSize, String gamerLogin) {
-		Pageable pageable = createPageable(pageNumber, pageSize);
+	public Page<GamerPublicResponseDto> findAllGamersPublicInfo(@Min(MIN_PAGE_NUMBER) Integer optionalPageNumber,
+																															@Min(MIN_PAGE_SIZE) @Max(MAX_PAGE_SIZE) Integer optionalPageSize,
+																															GamerSortOption optionalSort, String optionalGamerLogin
+	) {
+		Pageable pageable = createPageable(optionalPageNumber, optionalPageSize, optionalSort);
 		Page<Gamer> gamerPage;
 
-		if(StringUtils.hasText(gamerLogin)) {
-			gamerPage = listGamersByLogin(gamerLogin, pageable);
+		if (StringUtils.hasText(optionalGamerLogin)) {
+			gamerPage = listGamersByLogin(optionalGamerLogin, pageable);
 		} else {
 			gamerPage = gamerRepository.findAll(pageable);
 		}
@@ -79,11 +85,12 @@ public class GamerService {
 		return gamerPage.map(gamerMapper::toGamerPublicResponseDto);
 	}
 
-	private Pageable createPageable(Integer pageNumber, Integer pageSize) {
-		int finalPageNumber = pageNumber != null ? pageNumber : DEFAULT_PAGE_NUMBER;
-		int finalPageSize = pageSize != null ? pageSize : DEFAULT_PAGE_SIZE;
+	private Pageable createPageable(Integer optionalPageNumber, Integer optionalPageSize, GamerSortOption optionalSort) {
+		int pageNumber = optionalPageNumber != null ? optionalPageNumber : DEFAULT_PAGE_NUMBER;
+		int pageSize = optionalPageSize != null ? optionalPageSize : DEFAULT_PAGE_SIZE;
+		GamerSortOption sort = optionalSort != null ? optionalSort : DEFAULT_GAMER_SORT_OPTION;
 
-		return PageRequest.of(finalPageNumber, finalPageSize);
+		return PageRequest.of(pageNumber, pageSize, sort.getSort());
 	}
 
 	private Page<Gamer> listGamersByLogin(String gamerLogin, Pageable pageable) {
@@ -111,7 +118,7 @@ public class GamerService {
 
 	//TODO poprawa na optional
 	public Gamer getGamerReference(long gamerId) {
-		if(!gamerRepository.existsById(gamerId)) {
+		if (!gamerRepository.existsById(gamerId)) {
 			throw new GamerNotFoundException(gamerId);
 		}
 
@@ -132,7 +139,7 @@ public class GamerService {
 
 	public Optional<GamerPrivateResponseDto> tryUpdateGamerProfile(@Valid GamerUpdateProfileRequestDto updateProfileDto, long gamerId) {
 		Optional<Gamer> optionalGamer = gamerRepository.findById(gamerId);
-		if(optionalGamer.isEmpty()) {
+		if (optionalGamer.isEmpty()) {
 			return Optional.ofNullable(gamerMapper.toGamerPrivateResponseDto(null));
 		}
 		Gamer gamer = optionalGamer.get();
@@ -179,7 +186,7 @@ public class GamerService {
 
 	public Optional<GamerPrivateResponseDto> tryUpdatePartiallyGamerAuthenticationData(@Valid GamerUpdateAuthenticationDataRequestDto updateAuthDto, long gamerId) {
 		Optional<Gamer> optionalGamer = gamerRepository.findById(gamerId);
-		if(optionalGamer.isEmpty()) {
+		if (optionalGamer.isEmpty()) {
 			return Optional.ofNullable(gamerMapper.toGamerPrivateResponseDto(null));
 		}
 		Gamer gamer = optionalGamer.get();
@@ -188,14 +195,14 @@ public class GamerService {
 			throw new InvalidPasswordException();
 		}
 
-		if(updateAuthDto.newPassword() != null) {
+		if (updateAuthDto.newPassword() != null) {
 			throwExceptionIfNewPasswordIsSameAsPreviousOne(updateAuthDto.newPassword(), gamer.getPassword(), passwordEncoder);
 
 			String newPasswordEncoded = passwordEncoder.encode(updateAuthDto.newPassword());
 			gamer.setPassword(newPasswordEncoded);
 		}
 
-		if(updateAuthDto.email() != null) {
+		if (updateAuthDto.email() != null) {
 			String newEmail = updateAuthDto.email();
 			if (!gamer.getEmail().equals(updateAuthDto.email()) && gamerRepository.existsByEmail(newEmail)) {
 				throw new EmailAlreadyUsedException(newEmail);
@@ -218,7 +225,9 @@ public class GamerService {
 
 	public void deleteGamers(long gamerId) {
 		//TODO
-	};
+	}
+
+	;
 
 	public boolean tryDeleteGamer(long gamerId) {
 		if (!gamerRepository.existsById(gamerId)) {
@@ -230,7 +239,7 @@ public class GamerService {
 	}
 
 	public void throwExceptionAndLogoutIfAuthenticatedGamerNotFound(@NotNull Principal principal, @NotNull HttpServletRequest request,
-	                                                                @NotNull HttpServletResponse response) {
+																																	@NotNull HttpServletResponse response) {
 		long principalId = principalExtractor.extractIdFromPrincipal(principal);
 
 		if (!gamerRepository.existsById(principalId)) {
